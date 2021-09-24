8fa9a2b0ae0d635ef7cca1096e469bde56fa6897
Goodbyez <miradadesigual@gmail.com>
2021-09-24 02:44:04 +0200

    Done

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -4,8 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Movie;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Pagination\LengthAwarePaginator;
 
 class MovieController extends Controller
 {
